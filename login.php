ccec074b1999c6f36387dbe3366e8374946982b3
Develpjuan <juanandres78.jg@gmail.com>
2024-08-31 10:17:36 -0500

    se creo el archivo de login

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+include("conexion.php");
+
+$usu = $_POST["usuario"];
+$pass = $_POST["password"];
+$ro = $_POST["rol"];
+
+
+$queryusuario = mysqli_query($conex, "SELECT * FROM login WHERE usuario = '$usu' AND password = '$pass' AND rol = '$rol'");
+$nr = mysqli_num_rows($queryusuario);
+
+if ($nr == 1) {
+    if($rol == "Usuario") {
+        header("Location: user.php");
+    } else if ($rol == "Administrador") {
+        header("Location: admin.php");
+    }
+} else {
+    echo "<script> alert('Usuario, contraseña o rol incorrecto');window.location= 'index.html'</script>";
+}
